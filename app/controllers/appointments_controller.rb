db8b82bfc00358708cd36a4c2540feb5c5ab63bd
lagofelipe <lagofelipe@gmail.com>
2020-10-30 15:48:20 -0500

    done

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,10 @@
 class AppointmentsController < ApplicationController
 
+    def show
+        @appointment = Appointment.find(params[:id])
+        @doctor = Doctor.find(params[:id])
+        @patient = Patient.find(params[:id])
+    end
 
 
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,13 @@
 class PatientsController < ApplicationController
-    has_many :appointments 
-    has_many :doctors, through: :appointments
+   
+def index
+ @patients = Patient.all
+end
+
+def show
+@patient = Patient.find (params[:id])
+end
+
+
+
 end
